f8cf57cf32a38810811c1e6100ab4c409066a063
LunaXion00 <lunaof12@gmail.com>
2026-07-19 00:26:53 +0900

    댓글 API 서비스 구현

---
+++ src/services/commentApi.js
@@ -0,0 +1,55 @@
+import { request } from "../apiClient.js";
+
+export async function getCommentList({
+  postId,
+}) {
+  return request(
+    `/api/posts/${postId}/comments`,
+    {
+      method: "GET",
+    },
+  );
+}
+
+export async function postComment({
+  postId,
+  commentBody,
+}) {
+  return request(
+    `/api/posts/${postId}/comments`,
+    {
+      method: "POST",
+      body: {
+        commentBody,
+      },
+    },
+  );
+}
+
+export async function modifyComment({
+  postId,
+  commentId,
+  commentBody,
+}) {
+  return request(
+    `/api/posts/${postId}/comments/${commentId}`,
+    {
+      method: "PATCH",
+      body: {
+        commentBody,
+      },
+    },
+  );
+}
+
+export async function deleteComment({
+  postId,
+  commentId,
+}) {
+  return request(
+    `/api/posts/${postId}/comments/${commentId}`,
+    {
+      method: "DELETE",
+    },
+  );
+}
